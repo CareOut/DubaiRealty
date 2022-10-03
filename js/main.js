8a583026515547a8210485069dd06c3b2fdd4c62
Stanislav <9226303141@mail.ru>
2022-10-03 21:48:17 +0500

    add first page adaptive

--- js/main.js
+++ js/main.js
@@ -13,19 +13,79 @@ title.forEach((el) => {
   });
 });
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".projects-slider", {
   loop: true,
 
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 4,
 
   autoplay: {
     delay: 4000,
     stopOnLastSlide: false,
     disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      slidesPerGroup: 4,
+      slidesPerColumn: 2,
+    },
+    480: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      slidesPerGroup: 4,
+      slidesPerColumn: 2,
+    },
+    800: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+      slidesPerGroup: 4,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      slidesPerGroup: 4,
+    },
+  },
+});
+
+const slider = new Swiper(".feedback-slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  autoplay: false,
+  watchSlidesProgress: true,
+
+  slideClass: "feedback-slide",
+  slideActiveClass: "feedback-slide--visible",
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slidesPerGroup: 1,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      slidesPerGroup: 2,
+    },
   },
 });
